c74d46ea86a980dc5f10be5ee94400beb8ac5621
claudiuvilau <vilau_claudiu@yahoo.com>
2020-09-08 21:15:48 +0200

    configuration App : DB, log...App.java run ok

--- src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
+++ src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
@@ -1,51 +1,71 @@
 package com.parkit.parkingsystem.config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.sql.*;
-
 public class DataBaseConfig {
 
-    private static final Logger logger = LogManager.getLogger("DataBaseConfig");
+	private static final Logger logger = LogManager.getLogger("DataBaseConfig");
 
-    public Connection getConnection() throws ClassNotFoundException, SQLException {
-        logger.info("Create DB connection");
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/prod","root","rootroot");
-    }
+	public Connection getConnection() throws ClassNotFoundException, SQLException {
+		logger.info("Create DB connection");
+		Class.forName("com.mysql.cj.jdbc.Driver");
 
-    public void closeConnection(Connection con){
-        if(con!=null){
-            try {
-                con.close();
-                logger.info("Closing DB connection");
-            } catch (SQLException e) {
-                logger.error("Error while closing connection",e);
-            }
-        }
-    }
+		// pour éviter l'erreur message The server time zone value 'Paris, Madrid (heure
+		// d'été...
+		String url_timezone = "?serverTimezone=UTC";
 
-    public void closePreparedStatement(PreparedStatement ps) {
-        if(ps!=null){
-            try {
-                ps.close();
-                logger.info("Closing Prepared Statement");
-            } catch (SQLException e) {
-                logger.error("Error while closing prepared statement",e);
-            }
-        }
-    }
+		String url = "jdbc:mysql://localhost:3306/prod";
+		String user = "claudiu";
+		String mdp = "java1234*";
 
-    public void closeResultSet(ResultSet rs) {
-        if(rs!=null){
-            try {
-                rs.close();
-                logger.info("Closing Result Set");
-            } catch (SQLException e) {
-                logger.error("Error while closing result set",e);
-            }
-        }
-    }
+		// pour lancer le fichier Data.sql :
+		// Connection con = DriverManager.getConnection(url + url_timezone, user, mdp);
+		// ScriptRunner sr = new ScriptRunner(con);
+		// Creating a reader object
+		// Reader reader = new BufferedReader(new FileReader("resources/Data.sql"));
+		// Running the script
+		// sr.runScript(reader);
+
+		return DriverManager.getConnection(url + url_timezone, user, mdp);
+	}
+
+	public void closeConnection(Connection con) {
+		if (con != null) {
+			try {
+				con.close();
+				logger.info("Closing DB connection");
+			} catch (SQLException e) {
+				logger.error("Error while closing connection", e);
+			}
+		}
+	}
+
+	public void closePreparedStatement(PreparedStatement ps) {
+		if (ps != null) {
+			try {
+				ps.close();
+				logger.info("Closing Prepared Statement");
+			} catch (SQLException e) {
+				logger.error("Error while closing prepared statement", e);
+			}
+		}
+	}
+
+	public void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+				logger.info("Closing Result Set");
+			} catch (SQLException e) {
+				logger.error("Error while closing result set", e);
+			}
+		}
+	}
 }
