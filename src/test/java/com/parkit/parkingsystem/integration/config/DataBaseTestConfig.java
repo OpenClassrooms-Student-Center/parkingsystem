d365f7462f659c49114709335eaf3c253467b2a8
hallier <ahallier@isara.fr>
2021-01-28 10:34:21 +0100

    Fix SpotBugs : hard-coded password move connexion parameters into log4j.properties use of connexion parameters in DataBaseConfig.java and DataBaseTestConfig.java

--- src/test/java/com/parkit/parkingsystem/integration/config/DataBaseTestConfig.java
+++ src/test/java/com/parkit/parkingsystem/integration/config/DataBaseTestConfig.java
@@ -1,23 +1,34 @@
 package com.parkit.parkingsystem.integration.config;
 
 import com.parkit.parkingsystem.config.DataBaseConfig;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DataBaseTestConfig extends DataBaseConfig {
 
   private static final Logger logger = LogManager.getLogger("DataBaseTestConfig");
 
-  public Connection getConnection() throws ClassNotFoundException, SQLException {
+  public Connection getConnection() throws ClassNotFoundException, SQLException, IOException {
+    Properties properties = new Properties();
+    FileInputStream fis = new FileInputStream("src/main/resources/log4j.properties");
+    properties.load(fis);
+    String url = properties.getProperty("urltest");
+    String user = properties.getProperty("user");
+    String password = properties.getProperty("password");
+
     logger.info("Create DB connection");
     Class.forName("com.mysql.cj.jdbc.Driver");
-    return DriverManager.getConnection(
-            "jdbc:mysql://localhost:3306/test", "root", "rootroot");
+    return DriverManager.getConnection(url, user, password);
   }
 
   public void closeConnection(Connection con) {
