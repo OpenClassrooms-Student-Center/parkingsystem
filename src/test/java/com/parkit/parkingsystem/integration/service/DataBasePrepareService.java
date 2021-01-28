7aba4d1903e1e276db9d8d3fa9162ddaddd3dfbe
hallier <ahallier@isara.fr>
2021-01-28 14:52:51 +0100

    Fix SpotBugs : clean up stream or resource close stream or resource in finally block in DataBasePrepareService.java

--- src/test/java/com/parkit/parkingsystem/integration/service/DataBasePrepareService.java
+++ src/test/java/com/parkit/parkingsystem/integration/service/DataBasePrepareService.java
@@ -1,26 +1,36 @@
 package com.parkit.parkingsystem.integration.service;
 
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
+
+import java.lang.ref.PhantomReference;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class DataBasePrepareService {
 
   DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
 
   public void clearDataBaseEntries() {
+    PreparedStatement ps1 = null;
+    PreparedStatement ps2 = null;
     Connection connection = null;
     try {
       connection = dataBaseTestConfig.getConnection();
 
       //set parking entries to available
-      connection.prepareStatement("update parking set available = true").execute();
+      ps1 = connection.prepareStatement("update parking set available = true");
+      ps1.execute();
 
       //clear ticket entries;
-      connection.prepareStatement("truncate table ticket").execute();
+      ps2 = connection.prepareStatement("truncate table ticket");
+      ps2.execute();
 
     } catch (Exception e) {
       e.printStackTrace();
     } finally {
+      dataBaseTestConfig.closePreparedStatement(ps1);
+      dataBaseTestConfig.closePreparedStatement(ps2);
       dataBaseTestConfig.closeConnection(connection);
     }
   }
