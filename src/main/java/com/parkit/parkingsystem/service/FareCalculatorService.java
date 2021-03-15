376111133940d5631cd74917e135ae325011e81c
Nicolas BIANCUCCI <nico5310@gmail.com>
2021-03-15 16:18:43 +0100

    story2 5% discount for recurring users

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -30,5 +30,8 @@ public class FareCalculatorService {
 		default:
 			throw new IllegalArgumentException("Unkown Parking Type");
 		}
+		if (ticket.isDiscount())
+			ticket.setPrice(ticket.getPrice() * 0.95);
+
 	}
 }
