278c9b4e176d3ed4d8c30b4cb007d7c2f746b816
Nicolas BIANCUCCI <nico5310@gmail.com>
2021-03-26 14:57:50 +0100

    Fix Test + pom.xml

--- src/main/java/com/parkit/parkingsystem/model/ParkingSpot.java
+++ src/main/java/com/parkit/parkingsystem/model/ParkingSpot.java
@@ -6,103 +6,102 @@ import com.parkit.parkingsystem.constants.ParkingType;
  * this class permit to storage and retrieve values from DB
  *
  * @author Nicolas BIANCUCCI
- *
  */
 public class ParkingSpot {
-	/**
-	 * Parking spot ID
-	 */
-	private int number;
+    /**
+     * Parking spot ID
+     */
+    private int number;
 
-	/**
-	 * Parkingtype chose type vehicle
-	 */
-	private ParkingType parkingType;
+    /**
+     * Parkingtype chose type vehicle
+     */
+    private ParkingType parkingType;
 
-	/**
-	 * availability of slot to parking spot
-	 */
-	private boolean isAvailable;
+    /**
+     * availability of slot to parking spot
+     */
+    private boolean isAvailable;
 
-	/**
-	 * class constructor
-	 *
-	 * @param number      parking spot id
-	 * @param parkingType type of vehicle
-	 * @param isAvailable available parking spot or not
-	 */
-	public ParkingSpot(int number, ParkingType parkingType, boolean isAvailable) {
-		this.number = number;
-		this.parkingType = parkingType;
-		this.isAvailable = isAvailable;
-	}
+    /**
+     * class constructor
+     *
+     * @param number      parking spot id
+     * @param parkingType type of vehicle
+     * @param isAvailable available parking spot or not
+     */
+    public ParkingSpot(int number, ParkingType parkingType, boolean isAvailable) {
+        this.number = number;
+        this.parkingType = parkingType;
+        this.isAvailable = isAvailable;
+    }
 
-	/**
-	 * getter of the parkingspot id
-	 *
-	 * @return the unique number of a given parking spot
-	 */
-	public int getId() {
-		return number;
-	}
+    /**
+     * getter of the parkingspot id
+     *
+     * @return the unique number of a given parking spot
+     */
+    public int getId() {
+        return number;
+    }
 
-	/**
-	 * setter of the parkingspot id
-	 *
-	 * @param number is parkingspot id
-	 */
-	public void setId(int number) {
-		this.number = number;
-	}
+    /**
+     * setter of the parkingspot id
+     *
+     * @param number is parkingspot id
+     */
+    public void setId(int number) {
+        this.number = number;
+    }
 
-	/**
-	 * getter of parkingtype
-	 *
-	 * @return the appropriate parking type
-	 */
-	public ParkingType getParkingType() {
-		return parkingType;
-	}
+    /**
+     * getter of parkingtype
+     *
+     * @return the appropriate parking type
+     */
+    public ParkingType getParkingType() {
+        return parkingType;
+    }
 
-	/**
-	 * setter of the parking type
-	 *
-	 * @param parkingType the appropriate parking type
-	 */
-	public void setParkingType(ParkingType parkingType) {
-		this.parkingType = parkingType;
-	}
+    /**
+     * setter of the parking type
+     *
+     * @param parkingType the appropriate parking type
+     */
+    public void setParkingType(ParkingType parkingType) {
+        this.parkingType = parkingType;
+    }
 
-	/**
-	 * getter of parkingspot is available
-	 *
-	 * @return true if that parking spot is available
-	 */
-	public boolean isAvailable() {
-		return isAvailable;
-	}
+    /**
+     * getter of parkingspot is available
+     *
+     * @return true if that parking spot is available
+     */
+    public boolean isAvailable() {
+        return isAvailable;
+    }
 
-	/**
-	 * setter of the parkingspot is available
-	 *
-	 * @param available true if a given parkingspot is available false if busy
-	 */
-	public void setAvailable(boolean available) {
-		isAvailable = available;
-	}
+    /**
+     * setter of the parkingspot is available
+     *
+     * @param available true if a given parkingspot is available false if busy
+     */
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		ParkingSpot that = (ParkingSpot) o;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        ParkingSpot that = (ParkingSpot) o;
 		return number == that.number;
-	}
+    }
 
-	@Override
-	public int hashCode() {
-		return number;
-	}
+    @Override
+    public int hashCode() {
+        return number;
+    }
 }
--- src/main/java/com/parkit/parkingsystem/service/InteractiveShell.java
+++ src/main/java/com/parkit/parkingsystem/service/InteractiveShell.java
@@ -1,5 +1,6 @@
 package com.parkit.parkingsystem.service;
 
+import com.parkit.parkingsystem.config.DataBaseConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,58 +12,57 @@ import com.parkit.parkingsystem.util.InputReaderUtil;
  * this class allows interaction between the user and the parking system
  *
  * @author Nicolas BIANCUCCI
- *
  */
 public class InteractiveShell {
-	/**
-	 * InterractiveShell logger
-	 */
-	private static final Logger logger = LogManager.getLogger("InteractiveShell");
+    /**
+     * InterractiveShell logger
+     */
+    private static final Logger logger = LogManager.getLogger("InteractiveShell");
 
-	/**
-	 * method to load interface parking service
-	 */
-	public static void loadInterface() {
-		logger.info("App initialized!!!");
-		System.out.println("Welcome to Parking System!");
+    /**
+     * method to load interface parking service
+     */
+    public static void loadInterface() {
+        logger.info("App initialized!!!");
+        System.out.println("Welcome to Parking System!");
 
-		boolean continueApp = true;
-		InputReaderUtil inputReaderUtil = new InputReaderUtil();
-		ParkingSpotDAO parkingSpotDAO = new ParkingSpotDAO();
-		TicketDAO ticketDAO = new TicketDAO();
-		ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        boolean continueApp = true;
+        InputReaderUtil inputReaderUtil = new InputReaderUtil();
+        ParkingSpotDAO parkingSpotDAO = new ParkingSpotDAO();
+        TicketDAO ticketDAO = new TicketDAO(new DataBaseConfig());
+        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
 
-		while (continueApp) {
-			loadMenu();
-			int option = inputReaderUtil.readSelection();
-			switch (option) {
-			case 1: {
-				parkingService.processIncomingVehicle();
-				break;
-			}
-			case 2: {
-				parkingService.processExitingVehicle();
-				break;
-			}
-			case 3: {
-				System.out.println("Exiting from the system!");
-				continueApp = false;
-				break;
-			}
-			default:
-				System.out.println("Unsupported option. Please enter a number corresponding to the provided menu");
-			}
-		}
-	}
+        while (continueApp) {
+            loadMenu();
+            int option = inputReaderUtil.readSelection();
+            switch (option) {
+                case 1: {
+                    parkingService.processIncomingVehicle();
+                    break;
+                }
+                case 2: {
+                    parkingService.processExitingVehicle();
+                    break;
+                }
+                case 3: {
+                    System.out.println("Exiting from the system!");
+                    continueApp = false;
+                    break;
+                }
+                default:
+                    System.out.println("Unsupported option. Please enter a number corresponding to the provided menu");
+            }
+        }
+    }
 
-	/**
-	 * notification will displayed after lauching parking system
-	 */
-	private static void loadMenu() {
-		System.out.println("Please select an option. Simply enter the number to choose an action");
-		System.out.println("1 New Vehicle Entering - Allocate Parking Space");
-		System.out.println("2 Vehicle Exiting - Generate Ticket Price");
-		System.out.println("3 Shutdown System");
-	}
+    /**
+     * notification will displayed after lauching parking system
+     */
+    private static void loadMenu() {
+        System.out.println("Please select an option. Simply enter the number to choose an action");
+        System.out.println("1 New Vehicle Entering - Allocate Parking Space");
+        System.out.println("2 Vehicle Exiting - Generate Ticket Price");
+        System.out.println("3 Shutdown System");
+    }
 
 }
