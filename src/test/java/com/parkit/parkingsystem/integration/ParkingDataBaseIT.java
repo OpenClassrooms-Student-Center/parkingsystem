e3ef2e516d23e3b5a11e2933d095dc4a547edba6
LisaCarranqueRios <lisa.carranquerios@free.fr>
2021-05-04 13:35:32 +0200

    Improve Integration Tests

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -7,20 +7,23 @@ import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
 import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.apache.tomcat.jni.Time;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes=ParkingSpotDAO.class)
+@AutoConfigureMockMvc
 public class ParkingDataBaseIT {
 
     private static DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
@@ -30,7 +33,7 @@ public class ParkingDataBaseIT {
     public Ticket ticket = new Ticket();
 
     @MockBean
-    private static InputReaderUtil inputReaderUtil;
+    private InputReaderUtil inputReaderUtil;
 
     @BeforeAll
     private static void setUp() throws Exception{
@@ -41,9 +44,8 @@ public class ParkingDataBaseIT {
         dataBasePrepareService = new DataBasePrepareService();
     }
 
-
-    @AfterAll
-    private static void tearDown(){
+    @AfterEach
+    private  void tearDown(){
         dataBasePrepareService.clearDataBaseEntries();
     }
 
@@ -59,7 +61,22 @@ public class ParkingDataBaseIT {
         assertNotNull(ticketSavedInDB.getInTime());
         assertNotNull(ticketSavedInDB.getParkingSpot());
         assertFalse(ticketSavedInDB.getParkingSpot().isAvailable());
+    }
 
+    @Test
+    public void testCarExit() throws Exception {
+        when(inputReaderUtil.readSelection()).thenReturn(2);
+        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        this.ticket.setVehicleRegNumber("1111");
+        when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("1111");
+        parkingService.processIncomingVehicle();
+        TimeUnit.MILLISECONDS.sleep(100);
+        parkingService.processExitingVehicle();
+        Ticket ticketSavedInDB = ticketDAO.getTicket(ticket.getVehicleRegNumber());
+        assertNotNull(ticketSavedInDB);
+        assertNotNull(ticketSavedInDB.getOutTime());
+        assertNotNull(ticketSavedInDB.getParkingSpot());
+        assertNotNull(ticketSavedInDB.getPrice());
     }
 
 
